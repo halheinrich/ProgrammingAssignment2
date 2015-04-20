9624eb4412fa62e5e3f488e3cc2bdbbaf70bdb8e
Hal Heinrich <halheinrich@telusplanet.net>
2015-04-19 23:23:29 -0600

    Revert "Added cachematrix.R"
    This reverts commit 39db00a8ea82a0b98f7101efe0a40d264ec36740.

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,15 @@
-## These functions work together to cache a solve() solution for a matrix
-## m1 <- matrix(sample(1:100), 10, 10)
-## mcm1 <- makeCacheMatrix(m1)
-## im <- cacheSolve(mcm1)
+## Put comments here that give an overall description of what your
+## functions do
 
-## makeCacheMatrix creates a cacheable matrix for use by cacheSolve
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setsolve <- function(solve) m <<- solve
-  getsolve <- function() m
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+
 }
 
-## cacheSolve uses the cached solve if available. If not, one is created
-## for use next time.
+
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  m <- x$getsolve()
-  if(!is.null(m)) {
-    message("getting cached solve")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setsolve(m)
-  m
 }
-
