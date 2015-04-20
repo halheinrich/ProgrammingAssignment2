2a7c63967f41d116bb5191c21813babfcae1568c
Hal Heinrich <halheinrich@telusplanet.net>
2015-04-19 23:38:31 -0600

    Improved comments
    Added usage example.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions work together to cache a solve() solution for a matrix
 
-## Write a short comment describing this function
+## Here's a usage example
+## m1 <- matrix(sample(1:100), 10, 10)
+## mcm1 <- makeCacheMatrix(m1)
+## im <- cacheSolve(mcm1)
+
+## makeCacheMatrix creates a cacheable matrix for use by cacheSolve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve uses the cached solve if available. If not, one is created
+## for use next time.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached solve")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
